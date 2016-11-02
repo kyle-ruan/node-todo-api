e003e63d1ac7be74b30b725acc3d28a0cf95e76d
Kyle-Ruan <kyleryk123@gmail.com>
2016-11-02 21:31:51 +1100

    restructure mongoose, add create todo endpoint

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  const { text } = req.body;
+  const todo = new Todo({ text });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => console.log('Started on part 3000'));
