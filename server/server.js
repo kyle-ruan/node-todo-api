271f018f6157fe5aef2dd74c1bc5312a71ef1f22
Kyle-Ruan <kyleryk123@gmail.com>
2016-11-03 19:46:54 +1100

    get endpoint

--- server/server.js
+++ server/server.js
@@ -20,4 +20,14 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos });
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
 app.listen(3000, () => console.log('Started on part 3000'));
+
+module.exports = { app };
